49af4d5f80569c44e387d28c9286caa1204eb348
vovaphp <sahno023@gmail.com>
2022-09-30 16:57:25 +0300

    autoload create

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php
+spl_autoload_register(function ($className) {
+    $classFile = 'vendor/' . $className . '.php';
+    if (file_exists($classFile)) {
+        include_once $classFile;
+        return true;
+    }
+    return false;
+});
